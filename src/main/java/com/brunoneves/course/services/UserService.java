58658444afeea8ef65d022836876d378775378ce
Bruno_Neves <nevesbruno814@gmail.com>
2025-01-26 21:22:22 -0400

    User delete

--- src/main/java/com/brunoneves/course/services/UserService.java
+++ src/main/java/com/brunoneves/course/services/UserService.java
@@ -3,9 +3,11 @@ package com.brunoneves.course.services;
 import com.brunoneves.course.DTO.UserDTO;
 import com.brunoneves.course.entities.User;
 import com.brunoneves.course.repositories.UserRepository;
+import org.hibernate.JDBCException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,16 +18,23 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    public User findById(Long id){
-      return userRepository.findById(id).get();
+    public User findById(Long id) {
+        return userRepository.findById(id).get();
     }
 
-    public User insert(User user){
+    public User insert(User user) {
         return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        User user = findById(id);
+
+        if (user != null)
+            userRepository.delete(user);
     }
 
 }
