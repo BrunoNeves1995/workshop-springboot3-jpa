0d911f115a24a05b2c36e9aacad9c68ee2cc6871
Bruno_Neves <nevesbruno814@gmail.com>
2025-01-26 22:29:56 -0400

    Exception handling - findById

--- src/main/java/com/brunoneves/course/services/UserService.java
+++ src/main/java/com/brunoneves/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.brunoneves.course.services;
 import com.brunoneves.course.DTO.UserDTO;
 import com.brunoneves.course.entities.User;
 import com.brunoneves.course.repositories.UserRepository;
+import com.brunoneves.course.services.exceptions.ResourceNotFoundException;
 import org.hibernate.JDBCException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,8 @@ public class UserService {
     }
 
     public User findById(Long id) {
-        return userRepository.findById(id).get();
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
